24eb7f5c2f02c6e6286c724359195071041a2144
MKsdachaigri <MKsdachaigri@gmail.com>
2024-06-02 18:51:14 +0300

    Added shooting

--- Main.cpp
+++ Main.cpp
@@ -1,35 +1,36 @@
 #include <iostream>
+#include <conio.h>
 #include <SFML/Graphics.hpp>
 
 using namespace std;
 using namespace sf;
 
-float offsetX = 0, offsteY = 0;
+float offsetX = 0, offsetY = 0;
 
 const int H = 10;
-const int W = 20;
+const int W = 30;
 
 const int ts = 50;
 
 String Map[H] = {
-	"AAAAAAAAAAAAAAAAAAAA",
-	"A                  A",
-	"A                  A",
-	"A                  A",
-	"A                  A",
-	"A                  A",
-	"A       A          A",
-	"A     AAAA         A",
-	"A    AAAA A        A",
-	"AAAAAAAAAAAAAAAAAAAA" };
+	"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+	"A                            A",
+	"A                            A",
+	"A                            A",
+	"A           AAAA             A",
+	"A          A                 A",
+	"A       A                    A",
+	"A     AAAA                   A",
+	"A    AAAAA                   A",
+	"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" };
 
-int ground = 400;
+
 
 class Player {
 public:
 	float dx, dy;
 	FloatRect rect;
-	bool onGround;
+	bool onGround, rig;
 	Sprite sprite;
 	float curFrame;
 
@@ -41,6 +42,7 @@ public:
 
 		dx = dy = 0;
 		curFrame = 0;
+		rig = 1;
 	}
 
 	void update(float time) {
@@ -52,22 +54,22 @@ public:
 		}
 		rect.top += dy * time;
 		onGround = 0;
-		if (rect.left == 0)
-		{
-			rect.left = ts;
-		}
 		Collision(1);
-		if (rect.top > ground)
-		{
-			rect.top = ground;
-			dy = 0;
-			onGround = 1;
-		}
+
+		sprite.setPosition(rect.left - offsetX, rect.top - offsetY);
+
 			sprite.move(0.2 * time, 0);
-			if(dx>0)
-			sprite.setTextureRect(IntRect(56, 8, 26, 26));
-			if (dx < 0)  sprite.setTextureRect(IntRect(56 + 26, 8, -26, 26));
-			sprite.setPosition(rect.left, rect.top);
+			if (dx > 0)
+			{
+				sprite.setTextureRect(IntRect(56, 8, 26, 26));
+				rig = 1;
+			}
+			if (dx < 0)
+			{
+				sprite.setTextureRect(IntRect(56 + 26, 8, -26, 26));
+				rig = 0;
+			}
+			sprite.setPosition(rect.left - offsetX, rect.top - offsetY);
 			dx = 0;
 	}
 	void Collision(float dir)
@@ -103,12 +105,57 @@ public:
 	}
 };
 
+class Bullet {
+public:
+	float dx, dy;
+	FloatRect rect;
+	int go;
+	Sprite sprite;
+
+	Bullet(Texture& image)
+	{
+		sprite.setTexture(image);
+		dx = dy = 0;
+		go = 0;
+	}
+	void update(float time)
+	{
+		rect.left += dx * time;
+		Collision(0);
+
+		rect.top += dy * time;
+		Collision(1);
+
+		sprite.setPosition(rect.left - offsetX, rect.top - offsetY);
+	}
+	void Collision(int dir)
+	{
+		for (int i = rect.top / ts; i < (rect.top + rect.height) / ts; i++)
+		{
+			for (int j = rect.left / ts; j < (rect.left + rect.width) / ts; j++)
+			{
+				if (Map[i][j] == 'A')
+				{
+					if ((dx > 0 && dir == 0) || (dx < 0 && dir == 0))
+					{
+						go = 0;
+					}
+				}
+			}
+		}
+	}
+};
+
 int main()
 {
 	RenderWindow window(VideoMode(1000, 500), "Mega Man");
 	Texture Picture;
 	Picture.loadFromFile("C:/random_game_files/finalsprites.png");
 	Player p (Picture);
+
+	Texture t1;
+	t1.loadFromFile("C:/random_game_files/bullet.png");
+	Bullet bul(t1);
 
 	Texture t2;
 	t2.loadFromFile("C:/random_game_files/platforms.png");
@@ -123,7 +170,10 @@ int main()
 		Event event;
 		while (window.pollEvent(event))
 		{
-			if (event.type == Event::Closed) window.close();
+			if (event.type == Event::Closed)
+			{
+				window.close();
+			}
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Left))
 		{
@@ -141,7 +191,48 @@ int main()
 				p.onGround = 0;
 			}
 		}
+		if (Keyboard::isKeyPressed(Keyboard::Z))
+		{
+			if (bul.go == 0)
+			{
+				if (p.rig)
+				{
+					bul.go = 1;
+					bul.rect.left = p.rect.left + 67;
+					bul.rect.top = p.rect.top + 20;
+				}
+				else
+				{
+					bul.go = 2;
+					bul.rect.left = p.rect.left - 12;
+					bul.rect.top = p.rect.top + 18;
+
+				}
+			}
+		}
+		if (bul.go == 0)
+		{
+			bul.rect.left = p.rect.left;
+			bul.rect.top = p.rect.top;
+		}
+		else if (bul.go == 1)
+		{
+			bul.dx = 1;
+		}
+		else if (bul.go == 2)
+		{
+			bul.dx = -1;
+		}
+
+
+
+		if(p.rect.left > 500 && p.rect.left < 1000)
+		offsetX = p.rect.left - 500;
+		if(p.rect.top > 500)
+		offsetY = p.rect.top - 250;
+
 		p.update(time);
+		bul.update(time);
 		window.clear(Color::White);
 
 		for (int i = 0; i < H; i++)
@@ -153,12 +244,16 @@ int main()
 					platform.setTextureRect(IntRect(0, 0, ts, ts));
 				}
 				if (Map[i][j] == ' ') continue;
-				platform.setPosition(j * ts, i * ts);
+				platform.setPosition(j * ts - offsetX, i * ts - offsetY);
 				window.draw(platform);
 			}
 		}
 
 		window.draw(p.sprite);
+		if (bul.go != 0)
+		{
+			window.draw(bul.sprite);
+		}
 		window.display();
 	}
 	return 0;
